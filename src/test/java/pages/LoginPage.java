e0491b6412864ee6c83f03dd1727b9724b1704a4
Shankark1990 <shankarkharade1990@gmail.com>
2025-04-23 18:05:24 +0530

    update

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,34 +3,41 @@ package pages;
 import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 import utilities.RepoObject;
 import utilities.UtilityActions;
 
 
 public class LoginPage {
     WebDriver driver;
+    @FindBy(xpath = RepoObject.username_xpath)
+    WebElement user_name;
 
     public LoginPage(WebDriver driver) {
         this.driver = driver;
+        PageFactory.initElements(driver, this);
     }
 
     public void enter_user_name(String username) {
-        driver.findElement(By.xpath(RepoObject.username_xpath)).sendKeys(username);
+        UtilityActions.sendText(user_name, username);
+//        driver.findElement(By.xpath(RepoObject.username_xpath)).sendKeys(username);
     }
 
     public void enter_password(String password) {
         driver.findElement(By.xpath(RepoObject.password_xpath)).sendKeys(password);
     }
 
-    public void click_login_button(){
+    public void click_login_button() {
         UtilityActions.eleClick(driver.findElement(By.xpath(RepoObject.loginBtn_xpath)));
     }
 
-    public void verify_order_success(){
-        try{
+    public void verify_order_success() {
+        try {
             System.out.println("Inside verify order success method");
             driver.findElement(By.xpath(RepoObject.finishBtn_xpath)).isDisplayed();
-        }catch (Exception e) {
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
     }
--- src/test/java/utilities/UtilityActions.java
+++ src/test/java/utilities/UtilityActions.java
@@ -4,10 +4,14 @@ import org.openqa.selenium.WebElement;
 
 public class UtilityActions {
 
-    public static void eleClick(WebElement ele){
+    public static void eleClick(WebElement ele) {
         ele.click();
     }
-    public static void sendText(WebElement ele, String txt){
+
+    public static void sendText(WebElement ele, String txt) {
         ele.sendKeys(txt);
     }
+
+
+
 }
